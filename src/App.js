60c796527bd0d87a793066d91ad7eb56986285c3
Mohit Maroliya <mmaroliya@gmail.com>
2021-12-12 06:06:20 +0530

    Adding the navbar

--- src/App.js
+++ src/App.js
@@ -1,13 +1,30 @@
 import './App.css';
-import loginPage from './components/loginPage';
-import navbar from './components/navbar';
-import signupPage from './components/signupPage';
+import Navbar from './components/Navbar';
+import Home from './components/Home';
+import About from './components/About';
+import SignupPage from './components/SignupPage';
+
+import {
+  BrowserRouter as Router,
+  Routes,
+  Route,
+} from "react-router-dom";
+
 
 
 function App() {
   return (
     <>
-      <loginPage />
+      <Router>
+      <Navbar />
+      <Routes>
+          <Route path="/" element={<Home />} />
+            
+          <Route exact path="/about" element={<About />} />
+           
+        </Routes>
+      </Router>
+      
     </>
   );
 }
---
+++ src/components/About.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const About = () => {
+    return (
+        <div>
+            <h1 className='text-center p-4 display-3'>This is About Page</h1>
+        </div>
+    )
+}
+
+export default About
