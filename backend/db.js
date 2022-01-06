96bf8e7a27caa2e1041a3b2f59c49f59a16c2360
Mohit Maroliya <mmaroliya@gmail.com>
2022-01-06 16:15:25 +0530

    Setup the remote mongo

--- backend/db.js
+++ backend/db.js
@@ -1,6 +1,6 @@
 const mongoose = require('mongoose');
-const mongoURL = "mongodb://localhost:27017/?readPreference=primary&appname=MongoDB%20Compass&ssl=false";
-
+//const mongoURL = "mongodb://localhost:27017/?readPreference=primary&appname=MongoDB%20Compass&ssl=false";
+const mongoURL = "mongodb+srv://mohit_maroliya:Zker42FNU.buD_E@authlogin-db-cluster.nrdf3.mongodb.net/myFirstDatabase?retryWrites=true&w=majority"
 const connectToMongo = async() => {
     mongoose.connect(mongoURL,  {useNewUrlParser: true}, ()=> {
         console.log("Connected to Mongo Successfully!");
